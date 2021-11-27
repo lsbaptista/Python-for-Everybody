3d7c01301742281337bf82f0a046f5407867c871
lsbaptista <34317858+lsbaptista@users.noreply.github.com>
2021-11-27 17:59:13 +0000

    exercises

--- PythonDataStructures.py
+++ PythonDataStructures.py
@@ -2,3 +2,21 @@
 
 for letter in 'banana' :
     print(letter)
+
+## First week
+#Strings
+# %%
+'a' in 'banana'
+# %%
+stuff='Hello World'
+dir(stuff)
+# %%
+word='banana'
+word.upper()
+# %%
+#Format operators
+camels=42
+'I have spotted %d camels.' % camels
+# %%
+'In %d years i have spotted %g %s.' % (3,0.1,'camels')
+# %%
