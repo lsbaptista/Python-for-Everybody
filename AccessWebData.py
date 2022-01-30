99f00c6cf396270de374a9c08d1eedf5f90aa39b
DaSilva <34317858+lsbaptista@users.noreply.github.com>
2022-01-30 16:02:32 +0000

    socket

--- AccessWebData.py
+++ AccessWebData.py
@@ -85,3 +85,20 @@ y=re.findall('[0-9]+',text) # one or more digits + one or more digits
 y=[int(i)for i in y]
 sum(y)
 # %%
+## NETWORKED TECHNOLOGY
+# Sockets in Python  (comunication)
+
+# %%
+import socket
+mysock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80)) # Host and port 80
+
+cmd= 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data=mysock.recv(512)
+    if (len(data)<1):
+        break
+    print(data.decode())
+mysock.close()
