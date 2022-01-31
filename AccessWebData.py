84f1ddfaa3bf2d9c9272e9065deb603cda6d1b57
DaSilva <34317858+lsbaptista@users.noreply.github.com>
2022-01-31 20:16:10 +0000

    Retrieving web pages

--- AccessWebData.py
+++ AccessWebData.py
@@ -93,7 +93,7 @@ import socket
 mysock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80)) # Host and port 80
 
-cmd= 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+cmd= 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
@@ -102,3 +102,17 @@ while True:
         break
     print(data.decode())
 mysock.close()
+
+# %%
+## Programs that Surf the Web
+print(ord('H'))
+
+# %%
+# Using urllib in Python
+
+import urllib.request, urllib.parse,urllib.error
+
+fhand=urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip())
+# %%
