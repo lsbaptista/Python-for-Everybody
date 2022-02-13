74b0b0787b9a7f7eb8de459c6c51c8a979226ac0
DaSilva <34317858+lsbaptista@users.noreply.github.com>
2022-02-13 15:51:33 +0000

    week4_assignment

--- AccessWebData.py
+++ AccessWebData.py
@@ -156,4 +156,100 @@ soup=bs(html,'html.parser')
 tags=soup('a')
 for tag in tags:
     print(tag.get('href', None))
+
+
+# %%
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+sum=0
+for tag in tags:
+    sum=sum+ int(tag.contents[0])
+print(sum)
+
+
+# %%
+##Scraping HTML Data with BeautifulSoup
+
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
+
+
+# %%
+
+## Assignment: Following Links in HTML Using BeautifulSoup
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+n=0
+while n<7:
+    tags = soup('a')
+    url = tags[17].get('href', None)
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    n=n+1
+
+mytag=tags[17]
+print('Contents:', mytag.contents)
+    
+
+
+
+
+
+
+
+
+
 # %%
