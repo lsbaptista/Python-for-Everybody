4fd584f35503c8370a2d15288cd619c7f8b42e70
DaSilva <34317858+lsbaptista@users.noreply.github.com>
2022-05-18 20:03:01 +0100

    new

--- databases.py
+++ databases.py
@@ -1,4 +1,5 @@
 # %%
+
 ## Using Databases with Python
 
 ## EmailDB
@@ -417,4 +418,183 @@ for entry in json_data:
         ( user_id, course_id ) )
 
     conn.commit()
+
+
+## ASSIGNEMENT WEEK 4
+# %%
+import json
+import sqlite3
+
+conn = sqlite3.connect('rosterDatadb.sqlite')
+cur = conn.cursor()
+
+# Do some setup
+cur.executescript('''
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Course;
+
+CREATE TABLE User (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name   TEXT UNIQUE
+);
+
+CREATE TABLE Course (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title  TEXT UNIQUE
+);
+
+CREATE TABLE Member (
+    user_id     INTEGER,
+    course_id   INTEGER,
+    role        INTEGER,
+    PRIMARY KEY (user_id, course_id)
+)
+''')
+
+# %%
+
+
+fname = input('Enter file name: ')
+if len(fname) < 1:
+    fname = 'roster_data.json'
+
+# [
+#   [ "Charley", "si110", 1 ],
+#   [ "Mea", "si110", 0 ],
+
+str_data = open(fname).read()
+json_data = json.loads(str_data)
+
+for entry in json_data:
+
+    name = entry[0]
+    title = entry[1]
+    role = entry[2] ## insert new value to get
+
+    print((name, title, role))
+
+    cur.execute('''INSERT OR IGNORE INTO User (name)
+        VALUES ( ? )''', ( name, ) )
+    cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
+    user_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Course (title)
+        VALUES ( ? )''', ( title, ) )
+    cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
+    course_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Member
+        (user_id, course_id, role) VALUES ( ?, ?, ?)''', # Insert "role"
+        ( user_id, course_id, role))
+
+    conn.commit()
+
+
+## WORKED EXAMPLE CHAPTER 15
+
+# %%
+## NOTE: need to use hidden.py and 
+import urllib.request, urllib.parse, urllib.error
+import twurl
+import json
+import sqlite3
+import ssl
+
+TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+
+conn = sqlite3.connect('friends.sqlite')
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS People
+            (id INTEGER PRIMARY KEY, name TEXT UNIQUE, retrieved INTEGER)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS Follows
+            (from_id INTEGER, to_id INTEGER, UNIQUE(from_id, to_id))''')
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    acct = input('Enter a Twitter account, or quit: ')
+    if (acct == 'quit'): break
+    if (len(acct) < 1):
+        cur.execute('SELECT id, name FROM People WHERE retrieved=0 LIMIT 1')
+        try:
+            (id, acct) = cur.fetchone()
+        except:
+            print('No unretrieved Twitter accounts found')
+            continue
+    else:
+        cur.execute('SELECT id FROM People WHERE name = ? LIMIT 1',
+                    (acct, ))
+        try:
+            id = cur.fetchone()[0]
+        except:
+            cur.execute('''INSERT OR IGNORE INTO People
+                        (name, retrieved) VALUES (?, 0)''', (acct, ))
+            conn.commit()
+            if cur.rowcount != 1:
+                print('Error inserting account:', acct)
+                continue
+            id = cur.lastrowid
+
+    url = twurl.augment(TWITTER_URL, {'screen_name': acct, 'count': '100'})
+    print('Retrieving account', acct)
+    try:
+        connection = urllib.request.urlopen(url, context=ctx)
+    except Exception as err:
+        print('Failed to Retrieve', err)
+        break
+
+    data = connection.read().decode()
+    headers = dict(connection.getheaders())
+
+    print('Remaining', headers['x-rate-limit-remaining'])
+
+    try:
+        js = json.loads(data)
+    except:
+        print('Unable to parse json')
+        print(data)
+        break
+
+    # Debugging
+    # print(json.dumps(js, indent=4))
+
+    if 'users' not in js:
+        print('Incorrect JSON received')
+        print(json.dumps(js, indent=4))
+        continue
+
+    cur.execute('UPDATE People SET retrieved=1 WHERE name = ?', (acct, ))
+
+    countnew = 0
+    countold = 0
+    for u in js['users']:
+        friend = u['screen_name']
+        print(friend)
+        cur.execute('SELECT id FROM People WHERE name = ? LIMIT 1',
+                    (friend, ))
+        try:
+            friend_id = cur.fetchone()[0]
+            countold = countold + 1
+        except:
+            cur.execute('''INSERT OR IGNORE INTO People (name, retrieved)
+                        VALUES (?, 0)''', (friend, ))
+            conn.commit()
+            if cur.rowcount != 1:
+                print('Error inserting account:', friend)
+                continue
+            friend_id = cur.lastrowid
+            countnew = countnew + 1
+        cur.execute('''INSERT OR IGNORE INTO Follows (from_id, to_id)
+                    VALUES (?, ?)''', (id, friend_id))
+    print('New accounts=', countnew, ' revisited=', countold)
+    print('Remaining', headers['x-rate-limit-remaining'])
+    conn.commit()
+cur.close()
+
+## WEEK 5 - GEOCODING
 # %%
